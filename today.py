8aa2eec48a4f5fc73385860d9ea8ddb6f6942dd1
Rene Salmon <renesalmon@Renes-MacBook.fritz.box>
2022-07-09 22:47:54 +0200

    improved style

---
+++ today.py
@@ -0,0 +1,13 @@
+'''
+This is the module docstring
+'''
+
+
+def my_name_is():
+    """A module computing my name"""
+    return "reneSalmon"
+
+
+def my_buddy_is():
+    """A module computing buddy pair names for the day"""
+    return "Vladimir Kosenkov"
